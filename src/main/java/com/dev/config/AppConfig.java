ac46b1bffd1f9bbb39e6f19b5285d4ace60cbb0a
Roman Dubovskyi <rdubowskij@gmail.com>
2020-02-14 13:31:51 +0200

    added HW31(db.properties)

--- src/main/java/com/dev/config/AppConfig.java
+++ src/main/java/com/dev/config/AppConfig.java
@@ -10,22 +10,33 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @Configuration
+@PropertySource("classpath:db.properties")
 @ComponentScan(basePackages = {"com.dev.dao",
         "com.dev.service"})
 public class AppConfig {
+    private final Environment environment;
+
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public LocalSessionFactoryBean getSessionFactory() {
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
         factoryBean.setDataSource(getDataSource());
 
         Properties properties = new Properties();
-        properties.put("show_sql", true);
-        properties.put("format_sql", true);
-        properties.put("use_sql_comments", true);
-        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("show_sql", environment.getProperty("show_sql"));
+        properties.put("format_sql", environment.getProperty("format_sql"));
+        properties.put("use_sql_comments", environment.getProperty("use_sql_comments"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl"
+                + ".auto"));
+
         factoryBean.setHibernateProperties(properties);
         factoryBean.setAnnotatedClasses(User.class);
         return factoryBean;
@@ -34,10 +45,10 @@ public class AppConfig {
     @Bean
     public DataSource getDataSource() {
         BasicDataSource source = new BasicDataSource();
-        source.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        source.setUrl("jdbc:mysql://localhost/spring?serverTimezone=UTC");
-        source.setUsername("root");
-        source.setPassword("roman");
+        source.setDriverClassName(environment.getProperty("db.driver"));
+        source.setUrl(environment.getProperty("db.url"));
+        source.setUsername(environment.getProperty("db.username"));
+        source.setPassword(environment.getProperty("db.password"));
         return source;
     }
 }
